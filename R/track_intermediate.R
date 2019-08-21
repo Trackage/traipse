7dcbad55ee2cba92bd0be15cc8c21441a00a6d74
Michael Sumner <mdsumner@gmail.com>
2019-08-21 12:20:47 +0000

    add duration to track_intermediate for #2

--- R/track_intermediate.R
+++ R/track_intermediate.R
@@ -20,32 +20,50 @@
 #' @param x longitude
 #' @param y latitude
 #' @param date optional input date-time in POSIXct
-#' @param distance optional minimum distance between input points
+#' @param distance optional minimum distance (metres) between interpolated points
+#' @param duration optional minimum duration (seconds) between interpolated point,
+#'  if set then `distance` must be `NULL` and `date` must be input
 #' @return a list of data frames of intermediate points (for use with `unnest()` from tidyr)
 #' @export
 #'
 #' @examples
 #' track_intermediate(trips0$x[1:10], trips0$y[1:10], distance = 15000)
 #'
-#' track_intermediate(trips0$x[1:10], trips0$y[1:10], date = trips0$date, distance = 1500)
+#' track_intermediate(trips0$x[1:10], trips0$y[1:10], date = trips0$date,
+#'                                  distance = 1500)
 #'
+#' inter_time <- track_intermediate(trips0$x[1:10], trips0$y[1:10],
+#'                             date = trips0$date, duration = 1800)
 #' \dontrun{
 #' ## run with full workflow to expand into a new data frame with
 #' ## `int_x`, `int_y`, and (optional) `int_date`
 #'  if (requireNamespace("tidyr") && requireNamespace("dplyr")) {
-#'    dd <- trips0 %>% group_by(id) %>%
-#'      mutate(inter = track_intermediate(x, y, date = date, distance = 50000)) %>%
+#'  tr1 <- trips0[seq(1, nrow(trips0), by = 30), ]
+#'    dd <- tr1 %>% group_by(id) %>%
+#'      mutate(inter = track_intermediate(x, y, date = date, distance = 150000)) %>%
 #'      tidyr::unnest()
-#'    plot(dd$int_date, dd$int_y)
-#'    abline(v = trips0$date)
+#'    plot(dd$int_date, dd$int_y, pch = ".", cex = 2, main = "equidistant in space")
+#'    abline(v = tr1$date)
+#'
+#'    dd1 <- tr1 %>% group_by(id) %>%
+#'      mutate(inter = track_intermediate(x, y, date = date, duration = 3600 * 12)) %>%
+#'      tidyr::unnest()
+#'    plot(dd1$int_date, dd1$int_y, pch = ".", cex = 2, main = "equispaced in time")
+#'    abline(v = tr1$date)
 #'  }
 #' }
-track_intermediate <- function(x, y, date = NULL, distance = NULL) {
+track_intermediate <- function(x, y, date = NULL, distance = NULL, duration = NULL) {
   n <- length(x)
+  if (!is.null(distance) && !is.null(duration)) stop("'distance' or 'duration' (or both) must be NULL")
   if (is.null(distance)) {
     npoints <- rep(15, n - 1)
   } else {
-    npoints <- pmax(3, ceiling(track_distance(x, y) / distance))[-1]
+    npoints <- pmax(3, ceiling(track_distance(x, y) / distance))[-1L]
+  }
+
+  if (!is.null(duration)) {
+    if (is.null(date)) stop("if 'duration' is not NULL, 'date' must also be given/n")
+    npoints <- pmax(3, ceiling(track_time(date) / duration))[-1L]
   }
   listm <- geosphere::gcIntermediate(cbind(x[-n], y[-n]), cbind(x[-1], y[-1]),
                                      n = npoints, addStartEnd = TRUE, sp = FALSE)
--- tests/testthat/test-intermediates.R
+++ tests/testthat/test-intermediates.R
@@ -1,4 +1,4 @@
-test_that("intermediate interpolation works", {
+test_that("intermediate interpolation in distance works", {
   expect_equivalent(round(track_intermediate(c(0, 0, 1, 0), c(0, 1, 1, 0),
                                       ISOdate(2001, 1, 1) + c(1:4))[[1]]$int_y, digits = 2L),
                     c(0, 0.06, 0.12, 0.19, 0.25, 0.31, 0.37, 0.44, 0.5, 0.56, 0.62,
@@ -21,4 +21,25 @@ test_that("intermediate interpolation works", {
                       147.44, 147.47, 147.5, 147.52, 147.55, 147.58, 147.61, 147.64,
                       147.66, 147.69, 147.72, 147.75, 147.77, 147.8, 147.83, 147.86,
                       147.89, 147.92, 147.94, 147.97, 148))
+})
+
+
+
+
+test_that("intermediate interpolation in time works", {
+  expect_equivalent(round(track_intermediate(c(0, 0, 1, 0), c(0, 1, 1, 0),
+                                             ISOdate(2001, 1, 1) + c(1:4) * 24 * 3600, duration = 1800)[[1]]$int_y, digits = 2L),
+                    c(0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14, 0.16, 0.18, 0.2,
+                      0.22, 0.24, 0.27, 0.29, 0.31, 0.33, 0.35, 0.37, 0.39, 0.41, 0.43,
+                      0.45, 0.47, 0.49, 0.51, 0.53, 0.55, 0.57, 0.59, 0.61, 0.63, 0.65,
+                      0.67, 0.69, 0.71, 0.73, 0.76, 0.78, 0.8, 0.82, 0.84, 0.86, 0.88,
+                      0.9, 0.92, 0.94, 0.96, 0.98, 1))
+
+
+})
+
+test_that("intermediate required arguments are sane", {
+  expect_error(track_intermediate(c(0, 0, 1, 0), c(0, 1, 1, 0), duration = 10000))
+  expect_error(track_intermediate(c(0, 0, 1, 0), c(0, 1, 1, 0), duration = 10000, distance = 50000))
+
 })
